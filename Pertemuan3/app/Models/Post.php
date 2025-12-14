80257c45785b7c71059b6b18ee030f7c70da965b
Rianda Rafqi <riandarafqi321@gmail.com>
2025-12-14 14:44:54 +0700

    mengerjakan tugas pertemuan 3

--- Pertemuan3/app/Models/Post.php
+++ Pertemuan3/app/Models/Post.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
